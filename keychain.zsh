9604c502b55559b4eb9de4dd1e23b1b30fbe8910
David Jones <dave@dkjones.org>
2013-02-25 23:02:11 -0500

    add comment to keychain file.

--- keychain.zsh
+++ keychain.zsh
@@ -1,6 +1,9 @@
+# Use keychain, if it is available.
+
 
 [[ -f $(which keychain 2> /dev/null) ]] && \
     keychain --nogui --quiet id_rsa
+
 [[ -f $HOME/.keychain/$HOSTNAME-sh ]] && \
     source $HOME/.keychain/$HOSTNAME-sh
 
