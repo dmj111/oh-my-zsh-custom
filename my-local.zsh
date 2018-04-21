5c9c27aaf03dcfd78d2e54b6d6288375ac37f4b8
Dave Jones <dmj111@gmail.com>
2018-04-21 12:43:54 -0400

    local updates

--- my-local.zsh
+++ my-local.zsh
@@ -1,10 +1,10 @@
 # load up any local files
 
-if [ -d ${ZSH_CUSTOM}/local ]; then 
+if [ -d ${ZSH_CUSTOM}/local ]; then
     for config_file (${ZSH_CUSTOM}/local/*.zsh(N)); do
         source $config_file
     done
     unset config_file
 fi
 
-    
+path=(/Users/dave/anaconda/bin $path)
